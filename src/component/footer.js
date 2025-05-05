3af128d51f84a61167b636270e4656c72b7f5bed
airwin67 <faitourdja@gmail.com>
2025-05-05 09:27:43 +0100

    updating footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -3,7 +3,7 @@ export class Footer extends React.Component{
     render(){
         return (
         <footer>
-            <img id="footer-img" src="images/LOGOb.png" alt="logo"/>
+            <img id="footer-img" src="./images/LOGOb.png" alt="logo"/>
             <p id="footer-txt">© 2020 Kasa. All rights reserved</p>
         </footer>
         )
